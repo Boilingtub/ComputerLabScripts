0148e57bca199eb4e3c13c42dab11a778a520ecc
Boilingtub <74053750+Boilingtub@users.noreply.github.com>
2023-10-29 10:49:12 +0200

    Add files via upload

---
+++ be.bat
@@ -0,0 +1,5 @@
+set CPLUS_INCLUDE_PATH = "C:\Users\jhbri\Coding\C++\BreadEngine\src\glfw-3.3.8\include"
+set LD_LIBRARY_PATH = ""
+wt -d "C:/Users/jhbri/Coding/C++/BreadEngine/build" cmd /k nvim -p "build.cpp" -p "../src/main.cpp"
+cd "C:/Users/jhbri/Coding/C++/BreadEngine/build" 
+
